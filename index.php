71317c0ada44c9073b25033b637a5d242a0e78fb
Zachary F Pitts <zacharyp@adara.cs.pdx.edu>
2009-11-16 14:30:40 -0800

    newuser added, but not useful yet

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 <html>
 <head>
 <title>arik181</title>
-	<meta name="author" content="">
-	<meta name="date" content="2008-11-08T20:32:31-0800">
-	<meta name="copyright" content="">
-	<meta name="keywords" content="">
-	<meta name="description" content="">
-	<meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-	<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
-	<meta http-equiv="content-style-type" content="text/css">
-	<meta http-equiv="expires" content="0">
-	<link type="text/css" title="blue" rel="stylesheet" href="blue.css">
+ <meta name="author" content="">
+ <meta name="date" content="2008-11-08T20:32:31-0800">
+ <meta name="copyright" content="">
+ <meta name="keywords" content="">
+ <meta name="description" content="">
+ <meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
+ <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+ <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
+ <meta http-equiv="content-style-type" content="text/css">
+ <meta http-equiv="expires" content="0">
+ <link type="text/css" title="blue" rel="stylesheet" href="blue.css">
 </head>
 <body>
 <div align="left" class="leftbottom"> </div>
@@ -25,26 +25,31 @@
 </div>
 <p>
 <div align="left" style="body">
-	<span class="whitek">K<br></span>
-	<span class="body meshadow1">Komrade Keeper<br></span>
-	<span class="body metext">Komrade Keeper<br></span>
-	<div align="left" class="bordershadow main1shadow"> </div>
-	<div align="left" class="border main1">
-		<div align="left" class="listinput sub">
-			<form action="login.php" method="POST">
-				<table border=0 cellpadding=4 cellspacing=4>
-				<tr>
-				<td>
-					Username: <td><input type="text" name="username"></td><br>
-				</td>
-				<tr>
-				<td>
-					Password: <td><input type="password" name="passwd"></td><br>
-				<tr>
-					<td></td><td align="right"> <input type="submit" value="Log In"> </td>
-				</table>
-			</form>
-		</div>
-	</div>
+ <span class="whitek">K<br></span>
+ <span class="body meshadow1">Komrade Keeper<br></span>
+ <span class="body metext">Komrade Keeper<br></span>
+ <div align="left" class="bordershadow main1shadow"> </div>
+ <div align="left" class="border main1">
+  <div align="left" class="listinput sub">
+   <form action="login.php" method="POST">
+    <table border=0 cellpadding=4 cellspacing=4>
+     <tr>
+      <td>
+      Username: <td><input type="text" name="username"></td><br>
+      </td>
+     <tr>
+      <td>
+      Password: <td><input type="password" name="passwd"></td><br>
+     <tr>
+      <td />
+      <td align="left"><a href=newuser.php>New User?</a> </td>
+      <td align="left"> <input type="submit" value="Log In"> </td>
+     </tr>
+     <tr>
+     </tr>
+    </table>
+   </form>
+  </div>
+ </div>
 </div></div><p></body></html>
 
---
+++ newuser.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<title>New User</title>
+	<meta name="author" content="">
+	<meta name="date" content="2008-11-08T20:32:31-0800">
+	<meta name="copyright" content="">
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+	<meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
+	<meta http-equiv="content-style-type" content="text/css">
+	<meta http-equiv="expires" content="0">
+	<link type="text/css" title="blue" rel="stylesheet" href="blue.css">
+</head>
+<body>
+<div align="left" class="leftbottom"> </div>
+<div align="left" class="topbar"> </div>
+<div align="left" class="rightbar"> </div>
+<div align="left" class="nametag1"> </div>
+<div align="left" class="nametag2"> </div>
+
+<div align="center" class="lefttop">
+</div>
+<p>
+<div align="left" style="body">
+	<span class="whitek">K<br></span>
+	<span class="body meshadow1">Komrade Keeper<br></span>
+	<span class="body metext">Komrade Keeper<br></span>
+	<div align="left" class="bordershadow main1shadow"> </div>
+	<div align="left" class="border main1">
+		<div align="left" class="listinput sub">
+			<form action="login.php" method="POST">
+				<table border=0 cellpadding=4 cellspacing=4>
+				<tr>
+				<td>
+					Username: <td><input type="text" name="username"></td><br>
+				</td>
+				<tr>
+				<td>
+					Password: <td><input type="password" name="passwd"></td><br>
+				<tr>
+					<td></td><td align="right"> <input type="submit" value="Log In"> </td>
+				</tr>
+				</table>
+			</form>
+		</div>
+	</div>
+</div></div><p></body></html>
+
