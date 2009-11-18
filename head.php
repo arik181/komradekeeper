eb6c8fb7ddf5eccd66cbb99883d5f684c25e9707
Devin A Quirozoliver <arik182@chandra.cs.pdx.edu>
2009-11-17 23:18:22 -0800

    Broke out html head and tail into include files.

---
+++ head.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<title>Komrade Keeper</title>
+ <meta name="author" content="">
+ <meta name="date" content="2008-11-08T20:32:31-0800">
+ <meta name="copyright" content="">
+ <meta name="keywords" content="">
+ <meta name="description" content="">
+ <meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
+ <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+ <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
+ <meta http-equiv="content-style-type" content="text/css">
+ <meta http-equiv="expires" content="0">
+ <link type="text/css" title="blue" rel="stylesheet" href="blue.css">
+</head>
+<body>
