4962bd8f201332e225fa6116d0d7def5d55b5dc6
Devin A Quirozoliver <arik182@chandra.cs.pdx.edu>
2009-11-19 09:58:22 -0800

    Touched menu.php and greeting.php (include files), and put in a framework (broken) for welcome.php, which will serve as the "main" page

--- welcome.php
+++ welcome.php
@@ -1,1 +1,31 @@
-i
+<?php
+ include('head.php');
+ include('menu.php');
+ include('greeting.php');
+
+ if ($_SERVER['REQUEST_METHOD'] != 'POST')
+ {
+   $me = $_SERVER['PHP_SELF'];
+?>
+
+
+<?php
+
+ } else
+ {
+  include('connect.php');
+
+    // Insert the new contact
+    $query = "
+    INSERT INTO users
+    VALUES ( nextval('contactidseq'),'" . $_POST['name'] . "','" . $_POST['address'] . "','" . $_POST['city'] . "','" . $_POST['state'] . "','" . $_POST['zip'] . "');";
+    $result = pg_query($connection, $query);
+  }
+
+  // Close db connection
+  if ($connection) { pg_close($connection); }
+
+ }
+ include('tail.php');
+?>
+
