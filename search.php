6663085356f5e1a72727e7572443b333c52928f7
Devin A Quirozoliver <arik182@chandra.cs.pdx.edu>
2009-11-21 00:10:35 -0800

    Search is mostly in place. Search by organization will have to be special cased. Havent

--- search.php
+++ search.php
@@ -41,10 +41,6 @@
       <td align="right"><input type="text" name="zip"></td>
      </tr>
      <tr>
-      <td> Organization: </td>
-      <td align="right"><input type="text" name="org"></td>
-     </tr>
-     <tr>
       <td colspan=2 align="right"> <input type="submit" value="Search Contacts"> </td>
      </tr>
     </table>
@@ -58,16 +54,81 @@
  {
   include('connect.php');
 
-    // Insert the new contact
-    $query = "
-    INSERT INTO users
-    VALUES ( nextval('contactidseq'),'" . $_POST['name'] . "','" . $_POST['address'] . "','" . $_POST['city'] . "','" . $_POST['state'] . "','" . $_POST['zip'] . "');";
-    $result = pg_query($connection, $query);
+  $queryparts[0] = $_POST['name'];
+  $queryparts[1] = $_POST['address'];
+  $queryparts[2] = $_POST['city'];
+  $queryparts[3] = $_POST['state'];
+  $queryparts[4] = $_POST['zip'];
+
+  $queryname[0] = "name";
+  $queryname[1] = "address";
+  $queryname[2] = "city";
+  $queryname[3] = "state";
+  $queryname[4] = "zip";
+
+  // Build the query
+  $query = "
+   SELECT * FROM contact 
+   WHERE ";
+
+  for ($i=0; $i<4; ++$i)
+  {
+    if ($queryparts[$i])
+    {
+      $query = $query . " " . $queryname[$i] . " LIKE '%" . $queryparts[$i] . "%'";
+
+      $j = $i+1;
+      while ($j < 4)
+      {
+        if ($queryparts[$j])
+        {
+	  $query = $query . " AND ";
+          break;
+        }
+        ++$j;
+      }
+    }
   }
+
+  $query = $query . ";";
+  //Debug print statement
+  echo $query;
+
+/*
+  // Do the search
+  $result = pg_query($connection, $query);
+  $colnum = pg_num_fields($result);
+
+  echo "<br>" . pg_num_rows($result) . " rows returned \n";
+  echo "<table border=1 cellpadding=4 cellspacing=2>\n<tr>\n";
+    
+  for ($i=0;$i<$colnum;++$i)
+  {
+   echo "<td>";
+   echo pg_field_name($result, $i);
+   echo "</td>";
+  }
+  echo "</tr><p>\n";
+  while ($row = pg_fetch_row($result)) 
+  {
+   echo "<tr>";
+   for ($i=0;$i<$colnum;++$i)
+   {
+    echo "<td>";
+    echo "$row[$i]";
+    echo "</td>\n";
+   }
+   echo "</tr>";
+  }
+  echo "</table>\n";
+  
+  echo "</div>\n</div>\n";
+
 
   // Close db connection
   if ($connection) { pg_close($connection); }
-
+*/
  include('tail.php');
+ }
 ?>
 
