d44cd3b6c95ed2007f064f12572063c8293f13f4
Devin A Quirozoliver <arik182@chandra.cs.pdx.edu>
2009-11-27 01:51:57 -0800

    Fixed a couple of minor redirect issues.

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <?php
 
- include('head.php');
 
  if ($_SERVER['REQUEST_METHOD'] != 'POST')
  {
 
-  include('loginform.php');
-  include('tail.php');
+  if (isset($_COOKIE['uname']))
+  {
+    include('head.php');
+    include('menu.php');
+    include('tail.php');
+  }
+  else
+  {
+    include('head.php');
+    include('loginform.php');
+    include('tail.php');
+  }
 
  }
 ?>
