2bb12d5a3f7cf809d1e0c720cde9484223bd2cd1
Zachary F Pitts <zacharyp@adara.cs.pdx.edu>
2009-12-02 11:11:17 -0800

    search for email added

--- search.php
+++ search.php
@@ -26,16 +26,20 @@
   $queryparts[3] = $_POST['state'];
   $queryparts[4] = $_POST['zip'];
 
-  $queryname[0] = "name";
-  $queryname[1] = "address";
-  $queryname[2] = "city";
-  $queryname[3] = "state";
-  $queryname[4] = "zip";
+  $queryname[0] = "c.name";
+  $queryname[1] = "c.address";
+  $queryname[2] = "c.city";
+  $queryname[3] = "c.state";
+  $queryname[4] = "c.zip";
 
   $query = "
-   SELECT contactid,name,address,city,state,zip FROM contact 
-   WHERE userid = '" . $_COOKIE["userid"] . "' ";  //Only show user's data
- 
+  SELECT c.contactid,c.name as \"Name\", c.address as \"Address\", c.city as \"City\",
+  c.state as \"State\", c.zip as \"Zip\", e.emailaddr as \"Email Address\"
+  FROM contact c JOIN users u ON c.userid = u.userid
+  LEFT OUTER JOIN email e ON c.contactid = e.contactid
+  WHERE u.name = '" . $_COOKIE['uname'] . "' AND u.password = '" . $_COOKIE['passwd'] .
+  "' AND c.userid = '" . $_COOKIE["userid"] . "' ";
+
   for ($i=0; $i<5; ++$i)
   {
     if ($queryparts[$i])
@@ -44,7 +48,7 @@
     }
   }
 
-  $query = $query . " ORDER by name;";
+  $query = $query . " ORDER by c.name;";
 
   //Debug print statement
   //echo $query;
